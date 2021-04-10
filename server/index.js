8a099c145693e8e812e53a6321fd178bf7362e2f
3A43Mka <3a43mka@gmail.com>
2021-04-10 18:49:11 +0300

    initial

---
+++ server/index.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+const express = require('express')
+const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
+app.use('/api', router)
+
+// Error handling
+app.use(errorHandler)
+
+app.use((req, res, next) => {
+  next({
+    status: 404,
+    message: 'Not Found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    return res.status(404).json({error: 404});
+  }
+
+  if (err.status === 500) {
+    return res.status(500).json({error: 500});
+  }
+
+  next();
+});
+
+
+const start = async () => {
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync()
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+
+start()
